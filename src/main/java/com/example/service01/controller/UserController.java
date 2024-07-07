e1ae0e0b00da399c91c7e45d32221e49854a799f
seize <cz2123zc@gmail.com>
2024-07-07 18:59:01 +0900

    깃에서 컨피그 가져 오도록

--- src/main/java/com/example/service01/controller/UserController.java
+++ src/main/java/com/example/service01/controller/UserController.java
@@ -2,6 +2,7 @@ package com.example.service01.controller;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -25,7 +26,7 @@ public class UserController {
         return "signup";
     }
 
-    @PostMapping("/info")
+    @GetMapping("/info")
     public String info() {
         log.info("info is being called");
         return "info";
